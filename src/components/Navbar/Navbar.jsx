9d2848f67b8db9a9f2f8355922d3e65c4014c9b6
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-28 21:48:40 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 import { GiHumbergerMenu } from "react-icons/gi"
 import { MdOutlineRestaurantMenu } from "react-icons/md"
-
+import images from "../../constants/images"
 import "./Navbar.css"
 
-const Navbar = () => <nav className="app_navbar">NavBar</nav>
+const Navbar = () => (
+  <nav className="app__navbar">
+    <div className="app_navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+  </nav>
+)
 
 export default Navbar

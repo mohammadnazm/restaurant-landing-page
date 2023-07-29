f7d44bb066d3c5a434f6a720866a259ba8eeab54
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 19:56:11 +0300

    Update Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading />
+      <h1 className="app__header-h1">The key to Fine Dining</h1>
     </div>
     <div className="app__wrapper_img"></div>
   </div>

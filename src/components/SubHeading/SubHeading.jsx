6f912a5e102ecc5801ec1eb8669ee5b98eab951c
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 19:49:49 +0300

    Update SubHeading.jsx

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -2,6 +2,10 @@ import React from "react"
 
 import { images } from "../../constants"
 
-const SubHeading = () => <div style={{ marginBottom: "1rem" }}></div>
+const SubHeading = () => (
+  <div style={{ marginBottom: "1rem" }}>
+    <p className="p__cormorant">TITLE</p>
+  </div>
+)
 
 export default SubHeading

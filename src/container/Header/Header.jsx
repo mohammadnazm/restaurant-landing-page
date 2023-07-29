10a747859aeccea90dad9d6a4623ef4990ac269e
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 23:09:51 +0300

    Update Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -14,11 +14,13 @@ const Header = () => (
         morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
         tellus
       </p>
-      <button type="button" className="custom__buttom">
+      <button type="button" className="custom__button">
         Explore Menu
       </button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 )
 

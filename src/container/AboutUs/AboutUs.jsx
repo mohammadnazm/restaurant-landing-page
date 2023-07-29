0c659323e63f5ddcb30de779e609ed13c42e4b79
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 23:53:27 +0300

    Update AboutUs.jsx

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react"
 
-import './AboutUs.css';
+import "./AboutUs.css"
 
 const AboutUs = () => (
-  <div>
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
     AboutUs
   </div>
-);
+)
 
-export default AboutUs;
+export default AboutUs

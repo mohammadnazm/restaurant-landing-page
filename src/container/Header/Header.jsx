6c46c4e886c0c982d6a563f19428165e221a5bea
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 23:03:09 +0300

    upgrade

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}></p>
     </div>
     <div className="app__wrapper_img"></div>
   </div>

805a410b55abf51da551034d52d59ce66ff84d5a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 17:07:14 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -44,32 +44,34 @@ const Navbar = () => {
             setToggleMenu(true)
           }}
         />
-        <div className="app__navbar-smallscreen-overlay flex__center slide-bottom">
-          <MdOutlineRestaurantMenu
-            fontSize={27}
-            className="overlay__close"
-            onClick={() => {
-              setToggleMenu(false)
-            }}
-          />
-          <ul className="app__navbar-smallscreen-links">
-            <li className="p__opensans">
-              <a href="#home">Home</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#about">About</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#menu">Menu</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#awards">Awards</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen-overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {
+                setToggleMenu(false)
+              }}
+            />
+            <ul className="app__navbar-smallscreen-links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   )

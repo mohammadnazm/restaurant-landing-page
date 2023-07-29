a080904258531cc33a15307948047bdd5fce76c2
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 19:25:43 +0300

    Update Header.jsx

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,6 +6,7 @@ import "./Header.css"
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info"></div>
+    <div className="app__wrapper_img"></div>
   </div>
 )
 

d05bd384145c3d3b5750e483011065dd029bac22
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-29 12:01:57 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -42,6 +42,23 @@ const Navbar = () => (
           className="overly-close"
           onClick={() => {}}
         />
+        <ul className="app__navbar-smallscreen">
+          <li className="p__opensans">
+            <a href="#home">Home</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#about">About</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#menu">Menu</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#awards">Awards</a>
+          </li>
+          <li className="p__opensans">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>

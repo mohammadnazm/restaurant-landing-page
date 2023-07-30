8a1c25b00cfb3c355a96c20d26139b4f1f4e6eee
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-30 18:19:01 +0300

    Update MenuItem.jsx

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -9,6 +9,10 @@ const MenuItem = ({ title, price, tags }) => (
         <p className="p__cormorant" style={{ color: "#DCCA87" }}>
           {title}
         </p>
+
+        <div className="app__menuitem-price">
+          <p className="p__cormorant">{price}</p>
+        </div>
       </div>
     </div>
   </div>

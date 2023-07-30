c55c7d6a279fcf39c73545bddf199b9bed6c68af
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-30 23:39:42 +0300

    Update Chef.jsx

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -4,6 +4,10 @@ import { SubHeading } from "../../components"
 import { images } from "../../constants"
 import "./Chef.css"
 
-const Chef = () => <div className="app__bg app__wrapper section__padding"></div>
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse"></div>
+  </div>
+)
 
 export default Chef

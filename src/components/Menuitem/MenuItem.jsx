d4b285d2181e067348c80b7cfcb9fcc34e7db834
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-30 21:17:27 +0300

    upgrade

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,26 +1,56 @@
 import React from "react"
 
+import { SubHeading, MenuItem } from "../../components"
+import { data, images } from "../../constants"
 import "./MenuItem.css"
 
-const MenuItem = ({ title, price, tags }) => (
-  <div className="app__menuitem">
-    <div className="app__menuitem-head">
-      <div className="app__menuitem-name">
-        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
-          {title}
-        </p>
-        <div className="app__menuitem-dash" />
-        <div className="app__menuitem-price">
-          <p className="p__cormorant">{price}</p>
+const SpecialMenu = () => (
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte" />
+      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
+    </div>
+
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine  flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine, index) => (
+            <MenuItem
+              key={wine.title + index}
+              title={wine.title}
+              price={wine.price}
+              tags={wine.tags}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu__img" />
+      </div>
+
+      <div className="app__specialMenu-menu_cocktails  flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem
+              key={cocktail.title + index}
+              title={cocktail.title}
+              price={cocktail.price}
+              tags={cocktail.tags}
+            />
+          ))}
         </div>
       </div>
     </div>
-    <div className="app__menuitem-sub">
-      <p className="p__opensanss" style={{ color: "#AAA" }}>
-        {tags}
-      </p>
+
+    <div style={{ marginTop: 15 }}>
+      <button type="button" className="custom__button">
+        View More
+      </button>
     </div>
   </div>
 )
 
-export default MenuItem
+export default SpecialMenu

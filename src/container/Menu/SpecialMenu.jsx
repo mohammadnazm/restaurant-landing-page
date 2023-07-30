521842de3e6cc079cd6d5c20d0b8f22894d288ba
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-30 14:43:48 +0300

    Update SpecialMenu.jsx

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -13,6 +13,11 @@ const SpecialMenu = () => (
       <div className="app__specialMenu-menu">
         <div className="app__specialMenu-menu_wine flex__center">
           <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+          <div className="app__specialMenu_menu_items">
+            {data.wines.map((wine, index) => {
+              ;<p>{wine.title}</p>
+            })}
+          </div>
         </div>
       </div>
     </div>

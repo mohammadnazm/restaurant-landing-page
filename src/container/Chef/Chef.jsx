d97b8ae91fa85e34b64eea30d035cda95f9b8c7a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-30 21:33:15 +0300

    Update Chef.jsx

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react"
 
-import './Chef.css';
+import { SubHeading } from "../../components"
+import { images } from "../../constants"
+import "./Chef.css"
 
-const Chef = () => (
-  <div>
-    Chef
-  </div>
-);
+const Chef = () => <div>Chef</div>
 
-export default Chef;
+export default Chef

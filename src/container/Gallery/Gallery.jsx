fd6fc714452f6218a86177687374a53d306db128
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 19:23:38 +0300

    Upgrade

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -53,11 +53,11 @@ const Gallery = () => {
       </div>
       <div className="app__gallery-images_arrows">
         <BsArrowLeftShort
-          className="gallery__arrow-icons"
+          className="gallery__arrow-icon"
           onClick={() => scroll("left")}
         />
         <BsArrowRightShort
-          className="gallery__arrow-icons"
+          className="gallery__arrow-icon"
           onClick={() => scroll("right")}
         />
       </div>

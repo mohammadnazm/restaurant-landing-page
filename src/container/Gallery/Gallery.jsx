79458d45f55d4b4e92c586b5c4428e22aa639b29
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 16:13:01 +0300

    Update Gallery.jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -7,6 +7,10 @@ const Gallery = () => (
     <div className="app__gallery-content">
       <SubHeading title="Instagram" />
       <h1 className="headtext-cormorant">Photo Gallery</h1>
+      <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis
+        ipsum turpis elit elit scelerisque egestas mu.
+      </p>
     </div>
   </div>
 )

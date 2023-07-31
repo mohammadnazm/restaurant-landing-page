8a43e9a2f9f44e9bb1faf9d45b6f7b75b278b0dc
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 16:09:25 +0300

    Update Gallery.jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -6,6 +6,7 @@ const Gallery = () => (
   <div className="app__gallery flex__center">
     <div className="app__gallery-content">
       <SubHeading title="Instagram" />
+      <h1 className="headtext-cormorant">Photo Gallery</h1>
     </div>
   </div>
 )

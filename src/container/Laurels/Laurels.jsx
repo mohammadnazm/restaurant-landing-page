0c96e157dfc627219a7d3cf94726ec6f3feed344
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 13:33:02 +0300

    Update Laurels.jsx

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react"
 
-import './Laurels.css';
+import { SubHeading } from "../../components"
+import { images, data } from "../../constants"
+import "./Laurels.css"
 
 const Laurels = () => (
-  <div>
-    Laurels
-  </div>
-);
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+    </div>
 
-export default Laurels;
+    <div className="app__wrapper_img"></div>
+  </div>
+)
+
+export default Laurels

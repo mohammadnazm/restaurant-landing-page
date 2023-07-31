130a5f8f81d56bb50055b4411a2029bd47922573
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 16:18:18 +0300

    Update Gallery.jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -2,20 +2,26 @@ import React from "react"
 import { SubHeading } from "../../components"
 import "./Gallery.css"
 
-const Gallery = () => (
-  <div className="app__gallery flex__center">
-    <div className="app__gallery-content">
-      <SubHeading title="Instagram" />
-      <h1 className="headtext-cormorant">Photo Gallery</h1>
-      <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis
-        ipsum turpis elit elit scelerisque egestas mu.
-      </p>
-      <button type="button" className="custom__button">
-        View More
-      </button>
+const Gallery = () => {
+  const scrollRef = React.useRef()
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext-cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}></div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Gallery

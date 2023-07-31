d80566011517429d96818d9b133481abc90ff86d
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 21:02:04 +0300

    Update FindUs.jsx

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -5,7 +5,12 @@ import { images } from "../../constants"
 
 const FindUs = () => (
   <div className="app__bg app__wrapper section__padding" id="contact">
-    <div className="app__wrapper_info"></div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 )
 

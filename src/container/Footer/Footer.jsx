ae586bc071de07584dd7988210a0a2ba535a8336
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 21:18:59 +0300

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -3,6 +3,11 @@ import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi"
 import { FooterOverlay, Newsletter } from "../../components"
 import "./Footer.css"
 
-const Footer = () => <div>Footer</div>
+const Footer = () => (
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+  </div>
+)
 
 export default Footer

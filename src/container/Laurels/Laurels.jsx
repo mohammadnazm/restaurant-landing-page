d48120faaac2e8e5775ebcf3e7e382db8270fe46
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-31 13:38:41 +0300

    Update Laurels.jsx

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -11,6 +11,12 @@ const Laurels = () => (
       <h1 className="headtext__cormorant">Our Laurels</h1>
     </div>
 
+    <div className="app__laurels_awards">
+      {data.awards.map(award => (
+        <AwardCard award={award} key={award.title} />
+      ))}
+    </div>
+
     <div className="app__wrapper_img"></div>
   </div>
 )

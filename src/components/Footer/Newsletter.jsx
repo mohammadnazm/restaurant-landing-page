5a47494b44a01cdd1ceac006b6f3d9507748dad0
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-01 10:57:33 +0300

    Upgrade

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react"
 
-import './Newsletter.css';
+import "./Newsletter.css"
+import SubHeading from "../SubHeading/SubHeading"
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+    </div>
   </div>
-);
+)
 
-export default Newsletter;
+export default Newsletter

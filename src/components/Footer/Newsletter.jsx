98f67a8e8a9089e5e19f066f555784173aa66d6a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-01 11:24:29 +0300

    Upgrade

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,18 +1,20 @@
 import React from "react"
 
-import "./Newsletter.css"
 import SubHeading from "../SubHeading/SubHeading"
+import "./Newsletter.css"
 
 const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
       <SubHeading title="Newsletter" />
-      <h1 className="headtext__cormorant">Subscribe to our newsletter</h1>
-      <p className="p__opensanss">And Never Miss Latest Updates</p>
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And never miss latest Updates!</p>
     </div>
     <div className="app__newsletter-input flex__center">
-      <input type="email" placeholder="Enter your e-mail address" />
-      <button className="custom__button">Subscribe</button>
+      <input type="email" placeholder="Enter your email address" />
+      <button type="button" className="custom__button">
+        Subscribe
+      </button>
     </div>
   </div>
 )

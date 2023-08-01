26a528f1538402af24b0927a75eca128587dc200
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-08-01 11:00:33 +0300

    Update Newsletter.jsx

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -7,6 +7,8 @@ const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
       <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe to our newsletter</h1>
+      <p className="p__opensanss">And Never Miss Latest Updates</p>
     </div>
   </div>
 )
